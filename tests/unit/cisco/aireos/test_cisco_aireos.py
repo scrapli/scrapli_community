639042e3710ea4307c92e7de8785e92ef4187253
Daniel Teycheney <danielfjteycheney@gmail.com>
2021-10-03 10:36:47 +1100

    Fixing regex, removing TODOs

--- tests/unit/cisco/aireos/test_cisco_aireos.py
+++ tests/unit/cisco/aireos/test_cisco_aireos.py
@@ -5,16 +5,21 @@ import pytest
 from scrapli_community.cisco.aireos.cisco_aireos import DEFAULT_PRIVILEGE_LEVELS
 
 
-# TODO
+
+
 @pytest.mark.parametrize(
     "priv_pattern",
     [
         ("exec", "(Cisco Controller) >"),
+        ("exec", "(something_fun) >"),
         ("configuration", "(Cisco Controller) config>"),
+        ("configuration", "(Its interestin) config>"),
     ],
     ids=[
         "base_prompt_exec",
+        "base_prompt_exec_non_default",
         "base_prompt_configuration",
+        "base_prompt_configuration_non_default",
     ],
 )
 def test_default_prompt_patterns(priv_pattern):
