5877048be19a5c751acd00d501ba07945ddc000e
Viktor Kertesz <74217042+viktorkertesz@users.noreply.github.com>
2021-11-10 15:41:53 +0100

    ``read_only`` variant added
    It may be needed if user has no privilege to go into config mode. Still show commands should work. Maybe this is better

--- scrapli_community/cisco/asa/cisco_asa.py
+++ scrapli_community/cisco/asa/cisco_asa.py
@@ -1,8 +1,12 @@
 """scrapli_community.cisco.asa.cisco_asa"""
 
 from scrapli.driver.network.base_driver import PrivilegeLevel
-from scrapli_community.cisco.asa.async_driver import default_async_on_close, default_async_on_open
-from scrapli_community.cisco.asa.sync_driver import default_sync_on_close, default_sync_on_open
+from scrapli_community.cisco.asa.async_driver import (
+    default_async_on_close, default_async_on_open, read_only_async_on_open
+)
+from scrapli_community.cisco.asa.sync_driver import (
+    default_sync_on_close, default_sync_on_open, read_only_sync_on_open
+)
 
 DEFAULT_PRIVILEGE_LEVELS = {
     "exec": (
@@ -57,4 +61,12 @@ SCRAPLI_PLATFORM = {
         "textfsm_platform": "cisco_asa",
         "genie_platform": "asa",
     },
+    "variants": {
+        "read_only": {
+            # Warning: terminal width may be default 80 which can interfere parsing!
+            # useful for users without config mode privilege
+            "sync_on_open": read_only_sync_on_open,
+            "async_on_open": read_only_async_on_open,
+        }
+    }
 }
