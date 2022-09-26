53f966fef386bb0beeece8346918ea0612ca27d2
Ilmari Aho <ilmari.aho@upcloud.com>
2022-09-26 17:10:11 +0300

    Cumulus Linux support added

---
+++ scrapli_community/cumulus/linux/cumulus_linux.py
@@ -0,0 +1,90 @@
+"""scrapli_community.cumulus.linux.cumulus_linux"""
+from scrapli.driver.network.base_driver import PrivilegeLevel
+from scrapli_community.cumulus.linux.async_driver import (
+    default_async_on_close,
+    default_async_on_open,
+)
+from scrapli_community.cumulus.linux.sync_driver import default_sync_on_close, default_sync_on_open
+
+DEFAULT_PRIVILEGE_LEVELS = {
+    "exec": (
+        PrivilegeLevel(
+            pattern=r"^\S+@\S+:\S+:\S+\$\s*$",
+            name="exec",
+            previous_priv="",
+            deescalate="",
+            escalate="",
+            escalate_auth=False,
+            escalate_prompt="",
+        )
+    ),
+    "configuration": (
+        PrivilegeLevel(
+            pattern=r"^\S+@\S+:\S+:\S+#\s*$",
+            name="configuration",
+            previous_priv="exec",
+            deescalate="exit",
+            escalate="sudo su",
+            escalate_auth=True,
+            escalate_prompt=": ",
+        )
+    ),
+}
+
+LOGIN_AS_ROOT = {
+    "exec": (
+        PrivilegeLevel(
+            pattern=r"^\S+@\S+:\S+:\S+#\s*$",
+            name="exec",
+            previous_priv="",
+            deescalate="",
+            escalate="",
+            escalate_auth=False,
+            escalate_prompt="",
+        )
+    ),
+    "configuration": (
+        PrivilegeLevel(
+            pattern=r"^\S+@\S+:\S+:\S+#\s*$",
+            name="configuration",
+            previous_priv="exec",
+            deescalate="",
+            escalate="",
+            escalate_auth=False,
+            escalate_prompt=": ",
+        )
+    ),
+}
+
+SCRAPLI_PLATFORM = {
+    "driver_type": "network",
+    "defaults": {
+        "privilege_levels": DEFAULT_PRIVILEGE_LEVELS,
+        "default_desired_privilege_level": "exec",
+        "sync_on_open": default_sync_on_open,
+        "async_on_open": default_async_on_open,
+        "sync_on_close": default_sync_on_close,
+        "async_on_close": default_async_on_close,
+        "failed_when_contains": [
+            "Permission denied",
+            "ERROR:",
+        ],
+        "textfsm_platform": "",
+        "genie_platform": "",
+    },
+    "variants": {
+        "root-login": {
+            "privilege_levels": LOGIN_AS_ROOT,
+            "sync_on_open": default_sync_on_open,
+            "async_on_open": default_async_on_open,
+            "sync_on_close": default_sync_on_close,
+            "async_on_close": default_async_on_close,
+            "failed_when_contains": [
+                "Permission denied",
+                "ERROR:",
+            ],
+            "textfsm_platform": "",
+            "genie_platform": "",
+        }
+    },
+}
---
+++ scrapli_community/cumulus/linux/sync_driver.py
@@ -0,0 +1,37 @@
+"""scrapli_community.cumulus.linux.sync_driver"""
+from scrapli.driver import NetworkDriver
+
+
+def default_sync_on_open(conn: NetworkDriver) -> None:
+    """
+    cumulus_linux on_open callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        None
+
+    Raises:
+        N/A
+    """
+    conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+
+
+def default_sync_on_close(conn: NetworkDriver) -> None:
+    """
+    cumulus_linux default on_close callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        None
+
+    Raises:
+        N/A
+
+    """
+    conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+    conn.channel.write(channel_input="exit")
+    conn.channel.send_return()
