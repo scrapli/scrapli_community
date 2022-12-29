953ec682adf4ae9fff51fc40fde6147d037c3fdd
Carl Montanari <carl.r.montanari@gmail.com>
2022-12-29 17:14:44 -0500

    chore: tidy up docstrings

--- scrapli_community/dlink/os/async_driver.py
+++ scrapli_community/dlink/os/async_driver.py
@@ -4,13 +4,17 @@ from scrapli.driver import AsyncNetworkDriver
 
 async def default_async_on_open(conn: AsyncNetworkDriver) -> None:
     """
-    Async scrapli_example default on_open callable
+    Async dlink_os default on_open callable
+
     Args:
         conn: AsyncNetworkDriver object
+
     Returns:
         N/A
+
     Raises:
         N/A
+
     """
     await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
     await conn.send_command(command="disable clipaging")
@@ -18,13 +22,17 @@ async def default_async_on_open(conn: AsyncNetworkDriver) -> None:
 
 async def default_async_on_close(conn: AsyncNetworkDriver) -> None:
     """
-    Async scrapli_example default on_close callable
+    Async dlink_os default on_close callable
+
     Args:
         conn: AsyncNetworkDriver object
+
     Returns:
         N/A
+
     Raises:
         N/A
+
     """
     # write exit directly to the transport as channel would fail to find the prompt after sending
     # the exit command!
