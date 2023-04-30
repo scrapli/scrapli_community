a856c37f1584cf98c27c5a040e1e6ac220d4398a
Ryan Gillespie <rgillespie@gmail.com>
2023-04-30 01:55:01 -0600

    palo fixes

--- scrapli_community/paloalto/panos/paloalto_panos.py
+++ scrapli_community/paloalto/panos/paloalto_panos.py
@@ -43,8 +43,9 @@ SCRAPLI_PLATFORM = {
         "async_on_close": default_async_on_close,
         "failed_when_contains": [
             "Unknown command:",
-            "Invalid Syntax.",
-            "Validation Error:",
+            "Invalid syntax.",
+            "Server error",
+            "Validation Error:",            
         ],
         "textfsm_platform": "paloalto_panos",
         "genie_platform": "",
