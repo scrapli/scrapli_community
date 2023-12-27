9e70b36933cd03c4af624b2a6eb2a9cbd4569626
Humberto Galiza <humbertogaliza@gmail.com>
2023-12-26 23:40:49 -0300

    Adding support for Datacom DmSwitch devices

---
+++ scrapli_community/datacom/dmswitch/async_driver.py
@@ -0,0 +1,67 @@
+"""scrapli_community.datacom.dmswitch.async_driver"""
+from typing import Any
+
+from scrapli.driver import AsyncNetworkDriver
+
+
+async def default_async_on_open(conn: AsyncNetworkDriver) -> None:
+    """
+    Async datacom_dmswitch default on_open callable
+
+    Args:
+        conn: AsyncNetworkDriver object
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+    await conn.send_command(command="configure")
+    await conn.send_command(command="no terminal paging")
+    await conn.send_command(command="exit")
+
+
+async def default_async_on_close(conn: AsyncNetworkDriver) -> None:
+    """
+    Async datacom_dmswitch default on_close callable
+
+    Args:
+        conn: AsyncNetworkDriver object
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+    conn.channel.write(channel_input="exit")
+    conn.channel.send_return()
+
+
+class AsyncDatacomDmSwitchDriver(AsyncNetworkDriver):
+    def __init__(self, **kwargs: Any) -> None:
+        """
+        Datacom dmswitch platform class
+
+        Args:
+            kwargs: keyword args
+
+        Returns:
+            N/A
+
+        Raises:
+            N/A
+
+        """
+        # *if* using anything but system transport pop out ptyprocess transport options, leaving
+        # anything else
+        transport_plugin = kwargs.get("transport", "system")
+        if transport_plugin != "system":
+            kwargs.get("transport_options", {}).pop("ptyprocess", None)
+
+        super().__init__(**kwargs)
