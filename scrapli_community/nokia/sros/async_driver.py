b4846ebc0c563a93f184746746fea1f1e7821f72
Michael Alvarez <michael_alvarez@claro.com.do>
2024-09-29 20:55:06 -0400

    feat(nokia_sros): Added a variant for Nokia DSLAMs, models: ARAM F, ARAM FD and ARAM D

--- scrapli_community/nokia/sros/async_driver.py
+++ scrapli_community/nokia/sros/async_driver.py
@@ -40,6 +40,26 @@ async def classic_default_async_on_open(conn: AsyncNetworkDriver) -> None:
     await conn.send_command(command="environment no more")
 
 
+async def classic_aram_async_on_open(conn: AsyncNetworkDriver) -> None:
+    """
+    nokia_sros aram on_open callable
+
+    Args:
+        conn: AsyncNetworkDriver object
+
+    Returns:
+        None
+
+    Raises:
+        N/A
+    """
+    await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+    await conn.send_command(command="environment mode batch")
+    await conn.send_command(command="environment inhibit-alarms")
+    await conn.send_command(command="environment print no-more")
+    await conn.send_command(command="exit all")
+
+
 async def default_async_on_close(conn: AsyncNetworkDriver) -> None:
     """
     nokia_sros default on_close callable
--- scrapli_community/nokia/sros/sync_driver.py
+++ scrapli_community/nokia/sros/sync_driver.py
@@ -40,6 +40,26 @@ def classic_default_sync_on_open(conn: NetworkDriver) -> None:
     conn.send_command(command="environment no more")
 
 
+def classic_aram_sync_on_open(conn: NetworkDriver) -> None:
+    """
+    nokia_sros aram mode on_open callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        None
+
+    Raises:
+        N/A
+    """
+    conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+    conn.send_command(command="environment mode batch")
+    conn.send_command(command="environment inhibit-alarms")
+    conn.send_command(command="environment print no-more")
+    conn.send_command(command="exit all")
+
+
 def default_sync_on_close(conn: NetworkDriver) -> None:
     """
     nokia_sros default on_close callable
